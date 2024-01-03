8a3804b85aa7ce9914ca1f111f5f7b07cb153c84
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-01-04 02:35:18 +0600

    added search and pagination

--- ecommerce_core/ecomapp/views.py
+++ ecommerce_core/ecomapp/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from .models import Product
 from django.views.generic.detail import DetailView
+from django.core.paginator import Paginator
 # Create your views here.
 def home(request):
     product_objects = Product.objects.all()
-    context = {
-        'product_objects': product_objects
-    }
-    return render(request, 'ecomapp/index.html', context )
+   #search
+    product_name = request.GET.get('product_name')
+
+    if product_name != '' and product_name is not None:
+        product_objects = product_objects.filter(title__icontains = product_name)
+    #paginator
+    paginator = Paginator(product_objects, 3)  # Show 10 contacts per page.
+    page = request.GET.get("page")
+    product_objects = paginator.get_page(page)
+        
+    return render(request, 'ecomapp/index.html', {'product_objects': product_objects} )
 
 
 class ProductDetailView(DetailView):
