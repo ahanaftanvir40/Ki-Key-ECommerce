e671113ed91fd8a3842c46adbe6ddc4803978cfb
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-01-08 08:20:50 +0600

    up

--- ecommerce_core/cart/views.py
+++ ecommerce_core/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from ecomapp.models import Product
 from django.http import JsonResponse
+
 # Create your views here.
 
 def cart_home(request):
